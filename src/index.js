baec94f03d86d9c7437b638f21de5db8f0d8e46a
Khubchand <khubchandbansal2@gmail.com>
2022-11-27 13:17:55 +0530

    comit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+
+import React,{useEffect} from 'react';
+import app from './firebase';
+import { getAuth,onAuthStateChanged} from "firebase/auth";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './Auth/Login';
+import Register from './Auth/Register';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Router>
+           <Routes>
+                 <Route exact path='/login' element={< Login />}></Route>
+                 <Route exact path='/register' element={< Register />}></Route>
+          </Routes>
 
+    </Router>
+
+  </React.StrictMode>
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
